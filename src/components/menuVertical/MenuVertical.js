44bfa6e1aee1b31dd8b4bd22800c6a2cf3a56262
Enri-malanquilla <enri.sl.malanquilla@gmail.com>
2021-12-27 19:36:55 +0100

    terminado html de la barra de navegacion

---
+++ src/components/menuVertical/MenuVertical.js
@@ -0,0 +1,43 @@
+export function MenuVertical() {
+  return (
+    <nav>
+      <ul>
+        <li className='navBar'></li>
+        <a href='/' className='link'>
+          <i data-feather='Home'></i>
+          <span>Home</span>
+        </a>
+        <li className='navBar'></li>
+        <a href='/' className='link'>
+          <i data-feather='Home'></i>
+          <span>Mensajes</span>
+        </a>
+        <li className='navBar'></li>
+        <a href='/' className='link'>
+          <i data-feather='Home'></i>
+          <span>Clientes</span>
+        </a>
+        <li className='navBar'></li>
+        <a href='/' className='link'>
+          <i data-feather='Home'></i>
+          <span>Proyectos</span>
+        </a>
+        <li className='navBar'></li>
+        <a href='/' className='link'>
+          <i data-feather='Home'></i>
+          <span>Búsquedas</span>
+        </a>
+        <li className='navBar'></li>
+        <a href='/' className='link'>
+          <i data-feather='Home'></i>
+          <span>Ayuda</span>
+        </a>
+        <li className='navBar'></li>
+        <a href='/' className='link'>
+          <i data-feather='Home'></i>
+          <span>Ajustes</span>
+        </a>
+      </ul>
+    </nav>
+  );
+}
